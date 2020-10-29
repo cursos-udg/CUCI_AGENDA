55597202d14f082d7aee368c83ef6d9d95b4c56e
octaviosanchez911 <octavio.sanchez@cuci.udg.mx>
2020-10-29 14:39:27 -0600

    update proyect

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
 from flask import Flask, render_template, request, redirect, url_for
 from consultas import *
 import json
+import os
 
-app = Flask(__name__)
+app = Flask(__name__ ,static_url_path="")
+port = os.getenv("PORT",8000)
 
 @app.route('/')
 def index():
-    return redirect(url_for("obtener_contactos"));
+    return redirect(url_for("obtener_contactos"))
 
 @app.route('/contactos')
 def obtener_contactos():
@@ -31,7 +33,6 @@ def detalles_prueba():
 @app.route('/detalles/<nombre>')
 def detalles_contacto(nombre):
     contacto = obtener_un_contacto(nombre)
-
     return render_template('detalles.html', contacto=contacto)
 
 @app.route('/contacto', methods=["POST", "GET"])
@@ -42,8 +43,6 @@ def contacto_form():
         insertar_un_contacto(request.form.to_dict())
         return redirect(url_for("obtener_contactos"))
 
-
-
 @app.route('/contacto/<nombre>', methods=["POST", "GET"])
 def contacto_form_editar(nombre):
     if request.method == "GET":
@@ -52,3 +51,6 @@ def contacto_form_editar(nombre):
     else:
         editar_un_contacto(nombre,request.form.to_dict())
         return redirect(url_for("detalles_contacto", nombre=nombre))
+
+if __name__ == "__main__":
+    app.run(port=port,host="0.0.0.0",debug=True)  
